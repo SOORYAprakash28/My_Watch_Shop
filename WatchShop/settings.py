2bd30c27e338cd700c18733f604c5dbbda84eb41
SOORYA PRAKASH ACHARYA <151657974+SOORYAprakash28@users.noreply.github.com>
2024-12-03 16:19:58 +0530

    Update settings.py

--- WatchShop/settings.py
+++ WatchShop/settings.py
@@ -126,9 +126,9 @@ STATIC_URL = 'static/'
 
 import os
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
 
 
 # Default primary key field type
